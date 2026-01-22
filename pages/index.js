c80351caf59408c423c8a6df4ac3766f91ff8549
Hoang Do <hoang.do.2102@gmail.com>
2026-01-22 14:55:42 +0700

    GeoGenetics IndpenedentDay

---
+++ pages/index.js
@@ -0,0 +1,19 @@
+import Head from 'next/head'
+import Image from 'next/image'
+import { Component } from "react" 
+
+export default class Main extends Component {
+	constructor(props){
+		super(props)
+	}	
+
+	componentDidMount(){
+
+	}
+  
+  	render(){
+  		return (
+  			<div>GeoGenetics</div>
+  		)
+  	}
+}
